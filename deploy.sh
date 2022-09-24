3483dab22e94ec751360301b2b030f6ceaefa514
Lencse <leventeloki@gmail.com>
2022-09-24 23:43:38 +0200

    Increased memory limit

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,6 @@
 
 set -e
 
-PUBSUB_TOPIC=test-topic-2
-GCLOUD_BUCKET=hbt-data-staging
-
-GCLOUD_PROJECT_STAGING=hungarian-blue-trail-staging
-GCLOUD_PROJECT_PROD=hungarian-blue-trail
-GCLOUD_REGION=europe-central2
-GCLOUD_FUNCTIONS_RUNTIME=nodejs16
-
-GRAPHQL_FUNCTION_NAME=graphql
-GRAPHQL_HANDLER_ENTRYPOINT=graphql
-GRAPHQL_MEMORY_CONFIG=1024MB
-GRAPHQL_TIMEOUT_CONFIG=30s
-GRAPHQL_MAX_INSTANCES_CONFIG=3
-
-WORKER_FUNCTION_NAME=worker
-WORKER_HANDLER_ENTRYPOINT=worker
-WORKER_MEMORY_CONFIG=256MB
-WORKER_TIMEOUT_CONFIG=540s
-WORKER_MAX_INSTANCES_CONFIG=2
-
 if [ "$GCLOUD_LOGGED_IN" != "true" ]; then
     echo ""
     echo "››› Login to gcloud"
@@ -45,6 +25,6 @@ gcloud functions deploy --gen2 convert \
     --trigger-http \
     --allow-unauthenticated \
     --timeout=30s \
-    --memory=1024MB \
+    --memory=2048MB \
     --set-env-vars=NODE_ENV=production \
     --max-instances=1
